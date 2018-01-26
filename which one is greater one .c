e9dcf0500347a87fc1510ca0aa0eb0e1f4051272
harirajasekrnrg <35653061+harirajasekrnrg@users.noreply.github.com>
2018-01-26 09:41:02 +0000

    Create which one is greater one .c

---
+++ which one is greater one .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b&&a>c)
+    printf("a is greater value");
+    
+    else if(b>c)
+    printf("b is greater value");
+    
+    else
+printf("c is greater");
+}
