fa292d4123a06d535e369a09f306ea65d5f0f5a9
harirajasekrnrg <35653061+harirajasekrnrg@users.noreply.github.com>
2018-02-27 16:01:24 +0530

    Create even numbers between intervals.c

---
+++ even numbers between intervals.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the interval");
+    scanf("%d %d",&a,&b);
+    for(c=a;c<=b;c++)
+    
+        if(c%2==0)
+        {
+        
+    printf("even numbers are:%d",c);
+
+}
+}
